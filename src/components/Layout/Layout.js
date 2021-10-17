d62f550d03abad6fb597b34dd1f864dfc21c3328
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-10-17 10:35:09 +0200

    Add www redirect

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 
 import Header from "@components/Header"
 import Footer from "@components/Footer"
 
 const Layout = ({ children, transparentHeader, showLandingMenu }) => {
+  useEffect(() => {
+    if (typeof window === "undefined") return
+
+    const wwwPattern = "www."
+
+    if (window.location.origin.includes(wwwPattern)) {
+      const { origin, pathname, search } = window.location
+      const url = `${origin.replace(wwwPattern, "")}${pathname}${search}`
+
+      window.location = url
+    }
+  }, [])
+
   return (
     <>
       <Header
