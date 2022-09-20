ce4bb5fcd5eb27fd628b774f464cca25d497ed3c
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-09-20 08:40:35 +0200

    Add party redirect page

---
+++ src/pages/devconvi-party.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+const DevconPartyRedirect: React.FC = () => {
+  useEffect(() => {
+    window.location.href = "https://www.eventbrite.com/e/biglietti-ethernas-community-party-413257784157"
+  }, [])
+
+  return (
+    <></>
+  )
+}
+
+export default DevconPartyRedirect
