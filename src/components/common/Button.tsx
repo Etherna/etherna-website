3bdd147fc5434f3e3a78280b3f5e4d446766240d
Mattia Dalzocchio <mattiadalzocchio@me.com>
2022-11-05 15:19:14 +0100

    add cta to dapp in hero section

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,48 +1,82 @@
-import React from "react"
+import React, { forwardRef } from "react"
 import classNames from "classnames"
 
 import classes from "@styles/components/common/Button.module.scss"
+import { Link } from "gatsby"
 
 type ButtonProps = {
   children?: React.ReactNode
   className?: string
   type?: "primary" | "secondary" | "danger" | "warning" | "info"
+  href?: string
+  target?: "_blank" | "_self" | "_parent" | "_top"
   disabled?: boolean
   large?: boolean
   submit?: boolean
   onClick?(): void
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button = forwardRef<HTMLElement, ButtonProps>(({
   children,
   className,
+  href,
+  target,
   type,
   large,
   disabled,
   submit,
   onClick
-}) => {
+}, ref) => {
+  const btnClassName = classNames(
+    classes.btn,
+    className,
+    {
+      [classes.btnPrimary]: type === "primary",
+      [classes.btnSecondary]: type === "secondary",
+      [classes.btnDanger]: type === "danger",
+      [classes.btnWarning]: type === "warning",
+      [classes.btnInfo]: type === "info",
+      [classes.large]: large,
+    }
+  )
+
+  if (href && href.startsWith("http")) {
+    return (
+      <a
+        className={btnClassName}
+        href={href}
+        target={target}
+        onClick={onClick}
+        ref={ref as any}
+      >
+        {children}
+      </a>
+    )
+  }
+  if (href && !href.startsWith("http")) {
+    return (
+      <Link
+        className={btnClassName}
+        to={href}
+        target={target}
+        onClick={onClick}
+      >
+        {children}
+      </Link>
+    )
+  }
+
   return (
     <button
-      className={classNames(
-        classes.btn,
-        className,
-        {
-          [classes.btnPrimary]: type === "primary",
-          [classes.btnSecondary]: type === "secondary",
-          [classes.btnDanger]: type === "danger",
-          [classes.btnWarning]: type === "warning",
-          [classes.btnInfo]: type === "info",
-          [classes.large]: large,
-        }
-      )}
+      className={btnClassName}
       type={submit ? "submit" : "button"}
       disabled={disabled}
       onClick={onClick}
+      ref={ref as any}
     >
       {children}
     </button>
   )
-}
+})
 
 export default Button
--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,25 +2,23 @@ import React, { useRef } from "react"
 import classNames from "classnames"
 
 import classes from "@styles/components/landing/Hero.module.scss"
-import { ReactComponent as ArrowDown } from "@images/icons/arrow-down.svg"
 
-import AnchorLink from "@components/common/AnchorLink"
 import Container from "@components/common/Container"
 import ViewportObserver from "@components/layout/ViewportObserver"
 import useLocale from "@context/locale-context/hooks/useLocale"
 import { useTranslations } from "@hooks/useTranslations"
+import Button from "@components/common/Button"
 
 const Hero: React.FC = () => {
   const titleRef = useRef<HTMLHeadingElement>(null)
   const subheadingRef = useRef<HTMLParagraphElement>(null)
-  const taglineRef = useRef<HTMLHeadingElement>(null)
-  const moreRef = useRef<HTMLDivElement>(null)
+  const ctaRef = useRef<HTMLButtonElement>(null)
   const [locale] = useLocale()
   const { t } = useTranslations(locale, "hero")
 
   return (
     <section className={classes.hero}>
-      <Container>
+      <Container className="flex flex-col items-center">
         <ViewportObserver childrenRef={titleRef} viewportClassName="animation-active">
           <h1
             className={classNames(classes.heroTitle, "fade-in-up")}
@@ -37,25 +35,10 @@ const Hero: React.FC = () => {
           />
         </ViewportObserver>
 
-        <ViewportObserver childrenRef={taglineRef} viewportClassName="animation-active">
-          <h3 className={classNames(classes.heroTagline, "fade-in-up", "delay-200")} ref={taglineRef}>
-            {t`tagline`}
-          </h3>
-        </ViewportObserver>
-
-        <ViewportObserver childrenRef={moreRef} viewportClassName="animation-active">
-          <div className="fade-in-up delay-300" ref={moreRef}>
-            <AnchorLink
-              to="transparency"
-              className={classes.heroCta}
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <ArrowDown />
-              {t`learnMore`}
-            </AnchorLink>
-          </div>
+        <ViewportObserver childrenRef={ctaRef} viewportClassName="animation-active">
+          <Button className="mt-8" type="primary" href="https://etherna.io" target="_blank" ref={ctaRef} large>
+            {t`visitOurDapp`}
+          </Button>
         </ViewportObserver>
       </Container>
     </section>
