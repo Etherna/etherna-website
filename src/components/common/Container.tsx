bc8b85d1b70479a529c7ed66c84b75402be60453
Mattia Dalzocchio <mattiadalzocchio@me.com>
2023-03-18 14:37:13 +0100

    add larger container for blog post

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -5,16 +5,17 @@ import type { PropsWithChildren } from "react"
 type ContainerProps = PropsWithChildren<{
   className?: string
   fluid?: boolean
+  larger?: boolean
 }>
 
-const Container: React.FC<ContainerProps> = ({ children, fluid, className }) => {
+const Container: React.FC<ContainerProps> = ({ children, fluid, larger, className }) => {
   return (
     <div
       className={classNames(
         "mx-auto w-full px-5",
         {
-          "xl:max-w-screen-xl; w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg":
-            !fluid,
+          "sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg": !fluid,
+          "xl:max-w-screen-xl": !fluid && larger,
         },
         className
       )}
