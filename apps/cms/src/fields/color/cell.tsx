7fc503eae0bbf17b7dc293623b5c91d23031f6c1
Mattia Dalzocchio <mattiadalzocchio@me.com>
2024-10-12 19:16:16 +0200

    fix color cell data

--- apps/cms/src/fields/color/cell.tsx
+++ apps/cms/src/fields/color/cell.tsx
@@ -1,11 +1,17 @@
-import type { DefaultCellComponentProps } from "payload"
+"use client"
 
-export function ColorCell(props: DefaultCellComponentProps) {
+import { useTableCell } from "@payloadcms/ui"
+
+import type { CellComponentProps } from "payload"
+
+export function ColorCell(_props: CellComponentProps) {
+  const cell = useTableCell()
+
   return (
     <div
       className="size-6 rounded"
       style={{
-        backgroundColor: props.cellData,
+        backgroundColor: cell.cellData,
       }}
     />
   )
