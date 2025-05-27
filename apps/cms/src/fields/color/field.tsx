03065b9939f3a5f3141f858da7d2c67443ebc136
Mattia Dalzocchio <mattiadalzocchio@me.com>
2025-05-27 21:47:12 +0200

    feat: app colors in cms color picker

--- apps/cms/src/fields/color/field.tsx
+++ apps/cms/src/fields/color/field.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import React from "react"
 import { HexColorPicker } from "react-colorful"
-import { Popup, TextInput, useField } from "@payloadcms/ui"
+import { Button, Popup, TextInput, useField } from "@payloadcms/ui"
 
 import { PaletteIcon } from "lucide-react"
 
@@ -24,9 +25,45 @@ const DefaultColorPalette = [
   "#FFFFFF", // White
 ]
 
+const AppColorPalette = [
+  "hsl(var(--background))",
+  "hsl(var(--foreground))",
+  "hsl(var(--card))",
+  "hsl(var(--card-foreground))",
+  "hsl(var(--popover))",
+  "hsl(var(--popover-foreground))",
+  "hsl(var(--primary))",
+  "hsl(var(--primary-foreground))",
+  "hsl(var(--secondary))",
+  "hsl(var(--secondary-foreground))",
+  "hsl(var(--muted))",
+  "hsl(var(--muted-foreground))",
+  "hsl(var(--accent))",
+  "hsl(var(--accent-foreground))",
+  "hsl(var(--success))",
+  "hsl(var(--success-foreground))",
+  "hsl(var(--info))",
+  "hsl(var(--info-foreground))",
+  "hsl(var(--warning))",
+  "hsl(var(--warning-foreground))",
+  "hsl(var(--gold))",
+  "hsl(var(--gold-foreground))",
+  "hsl(var(--destructive))",
+  "hsl(var(--destructive-foreground))",
+  "hsl(var(--border))",
+  "hsl(var(--input))",
+  "hsl(var(--ring))",
+  "hsl(var(--chart-1))",
+  "hsl(var(--chart-2))",
+  "hsl(var(--chart-3))",
+  "hsl(var(--chart-4))",
+  "hsl(var(--chart-5))",
+]
+
 export function ColorField({ field, readOnly, path }: TextFieldClientProps) {
   const { label } = field
   const { value, setValue } = useField<string>({ path })
+  const [tab, setTab] = React.useState<"default" | "app">("default")
 
   return (
     <div
@@ -57,27 +94,73 @@ export function ColorField({ field, readOnly, path }: TextFieldClientProps) {
         <Popup
           horizontalAlign="right"
           verticalAlign="bottom"
+          className=""
           button={<PaletteIcon className="size-6" />}
           disabled={readOnly}
         >
-          <HexColorPicker
-            className="mx-auto p-5"
-            color={value?.startsWith("#") ? value : ""}
-            onChange={setValue}
-          />
-          <div className="mt-4 grid grid-cols-7 gap-2">
-            {DefaultColorPalette.map((color) => (
-              <div
-                key={color}
-                className={`aspect-square w-full rounded-sm`}
-                onClick={() => setValue(color)}
-                style={{
-                  backgroundColor: color,
-                  boxShadow: value === color ? "0 0 0 2px var(--theme-elevation-900)" : "none",
-                }}
-              />
-            ))}
+          <div className="mb-4 flex w-full gap-1 px-2 [&>*]:flex-1">
+            <Button
+              size="small"
+              buttonStyle={tab === "default" ? "primary" : "subtle"}
+              onClick={() => setTab("default")}
+            >
+              Default
+            </Button>
+            <Button
+              size="small"
+              buttonStyle={tab === "app" ? "primary" : "subtle"}
+              onClick={() => setTab("app")}
+            >
+              App
+            </Button>
           </div>
+
+          {tab === "default" && (
+            <>
+              <HexColorPicker
+                className="mx-auto p-5"
+                color={value?.startsWith("#") ? value : ""}
+                onChange={setValue}
+              />
+              <div className="mt-4 grid grid-cols-7 gap-2 px-2">
+                {DefaultColorPalette.map((color) => (
+                  <div
+                    key={color}
+                    className={`aspect-square w-full rounded-sm`}
+                    onClick={() => setValue(color)}
+                    style={{
+                      backgroundColor: color,
+                      boxShadow: value === color ? "0 0 0 2px var(--theme-elevation-900)" : "none",
+                    }}
+                  />
+                ))}
+              </div>
+            </>
+          )}
+
+          {tab === "app" && (
+            <>
+              <div className="mt-4 flex max-h-[40vh] flex-col gap-2 overflow-auto px-2">
+                {AppColorPalette.map((color) => (
+                  <div
+                    key={color}
+                    className="flex cursor-default items-center gap-2"
+                    onClick={() => setValue(color)}
+                  >
+                    <div
+                      className={`aspect-square size-4 rounded-sm`}
+                      style={{
+                        backgroundColor: color,
+                        boxShadow:
+                          value === color ? "0 0 0 2px var(--theme-elevation-900)" : "none",
+                      }}
+                    />
+                    <span>{color}</span>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
         </Popup>
       </div>
     </div>
